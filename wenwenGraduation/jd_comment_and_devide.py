3803ac538c164cee9f97948d1a301d05f7b9b089
jhalan6 <1050482532@qq.com>
2017-04-22 18:36:21 +0800

    add positive/negative words in repostory and refactor code

--- wenwenGraduation/jd_comment_and_devide.py
+++ wenwenGraduation/jd_comment_and_devide.py
@@ -11,104 +11,138 @@ import sys
 import codecs
 import requests
 import pandas as pd
+from pprint import pprint
 
 
 def get_comments_from_jd():
-    l_cmt, l_time, l_like, l_score, l_reply, l_level, l_prov, l_nn, l_client = \
-        [], [], [], [], [], [], [], [], []
-    result_lines = []
-    pid = sys.argv[1]
-    pages = int(sys.argv[2])
+        l_cmt, l_time, l_like, l_score, l_reply, l_level, l_prov, l_nn, l_client = [
+                ], [], [], [], [], [], [], [], []
+        result_lines = []
+        pid = sys.argv[1]
+        pages = int(sys.argv[2])
 
-    for page in range(pages):  #12.13 edit
-        print "Page %i ..." % (page + 1)
-        url = "https://sclub.jd.com/comment/productPageComments.action?productId=" +\
-            pid + "&score=0&sortType=3&page=" + str(page) + \
-            "&pageSize=10&callback=fetchJSON_comment98vv37464"
+        for page in range(pages):  # 12.13 edit
+                print "Page %i ..." % (page + 1)
+                url = "https://sclub.jd.com/comment/productPageComments.action?productId=" +\
+                    pid + "&score=0&sortType=3&page=" + str(page) + \
+                    "&pageSize=10&callback=fetchJSON_comment98vv37464"
 
-        print url
-        ws1 = urllib2.urlopen(url).read()
-        groups = re.findall('''\{"id":\d+,.*?,"content":"((?:(?!<\/div>).|\n)*?)",.*?,"referenceTime":"(.*?)",.*?,"replyCount":(\d+),"score":(\d+),"status":\d,"title":"","usefulVoteCount":(\d+),.*?,"userLevelId":"(.*?)","userProvince":"(.*?)",.*?,"nickname":"(.*?)","userClient":(\d+),''', ws1)
-        for g in groups:
-            l_cmt.append(g[0])
-            l_time.append(g[1])
-            l_like.append(g[2])
-            l_score.append(g[3])
-            l_reply.append(g[4])
-            l_level.append(g[5])
-            l_prov.append(g[6])
-            l_nn.append(g[7])
-            l_client.append(g[8])
-            result_lines.append(("%s\n" % g[0]).decode('gbk', 'ignore').encode('utf-8'))
-        time.sleep(random.randint(5, 10) / 10.0)
+                print url
+                ws1 = urllib2.urlopen(url).read()
+                groups = re.findall(
+                    '''\{"id":\d+,.*?,"content":"((?:(?!<\/div>).|\n)*?)",.*?,"referenceTime":"(.*?)",.*?,"replyCount":(\d+),"score":(\d+),"status":\d,"title":"","usefulVoteCount":(\d+),.*?,"userLevelId":"(.*?)","userProvince":"(.*?)",.*?,"nickname":"(.*?)","userClient":(\d+),''',
+                    ws1)
+                for g in groups:
+                        l_cmt.append(g[0])
+                        l_time.append(g[1])
+                        l_like.append(g[2])
+                        l_score.append(g[3])
+                        l_reply.append(g[4])
+                        l_level.append(g[5])
+                        l_prov.append(g[6])
+                        l_nn.append(g[7])
+                        l_client.append(g[8])
+                        result_lines.append(
+                            ("%s\n" %
+                             g[0]).decode(
+                                'gbk',
+                                'ignore').encode('utf-8'))
+                time.sleep(random.randint(5, 10) / 10.0)
 
-    df = pd.DataFrame({"cmt": l_cmt,
-                       "time": l_time,
-                       "like": l_like,
-                       "score": l_score,
-                       "reply": l_reply,
-                       "level": l_level,
-                       "prov": l_prov,
-                       "nn": l_nn,
-                       "client": l_client})
-    df.to_csv('file.csv', index = False)
-    print "grep from jd done.."
-    write_to_file("contents.txt", result_lines)
-    print "write contents.txt done"
+        df = pd.DataFrame({"cmt": l_cmt,
+                           "time": l_time,
+                           "like": l_like,
+                           "score": l_score,
+                           "reply": l_reply,
+                           "level": l_level,
+                           "prov": l_prov,
+                           "nn": l_nn,
+                           "client": l_client})
+        df.to_csv('file.csv', index=False)
+        print "grep from jd done.."
+        write_to_file("contents.txt", result_lines)
+        print "write contents.txt done"
 
 
 def solve_encode():
-    reload(sys)
-    sys.setdefaultencoding("utf-8")
+        reload(sys)
+        sys.setdefaultencoding("utf-8")
 
 
-def devide_sentence_env():
-    global api_key
-    global format
-    global pattern
-    global url_get_base
-    api_key = 'i1o4P9M192j2l7y1U1I1lrhhyyeAaZrmTqdXhXsj'
-    format = 'json'
-    pattern = 'dp'
-    url_get_base = "http://ltpapi.voicecloud.cn/analysis/"
+def init_devide_sentence_env():
+        global api_key
+        global format
+        global pattern
+        global url_get_base
+        api_key = 'i1o4P9M192j2l7y1U1I1lrhhyyeAaZrmTqdXhXsj'
+        format = 'json'
+        pattern = 'dp'
+        url_get_base = "http://ltpapi.voicecloud.cn/analysis/"
 
 
 def write_to_file(file_name, lines):
-    f = codecs.open(file_name, "w", "utf-8")
-    for line in lines:
-        f.write(line)
-    f.close()
+        f = codecs.open(file_name, "w", "utf-8")
+        for line in lines:
+                f.write(line)
+        f.close()
+
+
+def fetch_all_words_from_yu_yin_yun():
+        global all_words
+        all_words = {}
+        for line in open("contents.txt"):
+                request_data = {
+                    "api_key": "i1o4P9M192j2l7y1U1I1lrhhyyeAaZrmTqdXhXsj",
+                    "text": unicode(line),
+                    "pattern": "ws",
+                    "format": "json"}
+
+                result = requests.post(url_get_base, data=request_data).content
+                content = result.strip()
+                js = json.loads(content)
+                devided_words = ""
+                for sent in js[0]:
+                        for data in sent:
+                                devided_words = "%s,%s" % (
+                                    devided_words, data['cont'])
+                all_words[line[:-1]] = devided_words
 
 
 def write_devide_sentence_and_content_to_file():
-    file_result = []
-    for line in open("contents.txt"):
-        # url = "%s?api_key=%s&text=%s&format=%s&pattern=%s" % \
-            # (url_get_base, api_key, line, format, pattern)
-        # result = requests.get(url).content
-        request_data = {"api_key": "i1o4P9M192j2l7y1U1I1lrhhyyeAaZrmTqdXhXsj",
-                        "text": unicode(line),
-                        "pattern": "ws",
-                        "format": "json"}
+        file_result = []
+        for sentence in all_words.keys():
+                file_result.append("%s|%s\n" % (sentence, all_words[sentence]))
+        write_to_file("contents.csv", file_result)
+        print "write to contents.csv done"
 
-        result = requests.post(url_get_base, data = request_data).content
-        content = result.strip()
-        js = json.loads(content)
-        devided_words = ""
-        for sent in js[0]:
-            for data in sent:
-                devided_words = "%s,%s" % (devided_words, data['cont'])
-        file_result.append("%s|%s\n" % (line[:-1], devided_words))
-    write_to_file("contents.csv", file_result)
-    print "write to contents.csv done"
+
+def filt_all_word_set():
+        global words_set
+        words_set = set()
+        for words in all_words.values():
+            for word in words.split(','):
+                words_set.add(word)
+
+
+def read_all_positive_words():
+        global positive_words
+        positive_words = set()
+        for line in open("positive_words.txt"):
+            positive_words.add(line)
+        for word in positive_words:
+            print word
+
 
 
 def main():
-    solve_encode()
-    get_comments_from_jd()
-    devide_sentence_env()
-    write_devide_sentence_and_content_to_file()
+        solve_encode()
+        get_comments_from_jd()
+        init_devide_sentence_env()
+        fetch_all_words_from_yu_yin_yun()
+        write_devide_sentence_and_content_to_file()
+        filt_all_word_set()
+        read_all_positive_words()
 
 
 if __name__ == '__main__':
-    main()
+        main()
